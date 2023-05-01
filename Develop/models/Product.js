6ff8e92bb71ba7493062276c1c0cfe28f9ce4534
Ben Smyth <118232312+bsmyth310@users.noreply.github.com>
2023-05-01 07:12:18 -0400

    Add files via upload
    Updated Models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,45 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+        notNull: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        notNull: true,
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
